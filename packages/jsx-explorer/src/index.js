6913a51ee86360f86c467f1e32180731390e7dc5
Amour1688 <lcz_1996@foxmail.com>
2020-06-14 16:18:50 +0800

    chore: console log ast in jsx explorer

--- packages/jsx-explorer/src/index.js
+++ packages/jsx-explorer/src/index.js
@@ -1,9 +1,16 @@
+/* eslint-disable no-console */
+// eslint-disable-next-line import/no-unresolved
 import * as monaco from 'monaco-editor';
+import { h, createApp } from 'vue';
 import { transform } from '@babel/core';
 import babelPluginJSx from '../../babel-plugin-jsx/src';
 import './index.css';
 // or import * as monaco from 'monaco-editor/esm/vs/editor/editor.api';
 // if shipping only a subset of the features & languages is desired
+
+createApp(
+  () => h('h1', null, 'Vue JSX Explorer'),
+).mount('#header');
 
 if (module.hot) {
   module.hot.accept('../../babel-plugin-jsx/src', () => {
@@ -50,11 +57,14 @@ const output = monaco.editor.create(document.getElementById('output'), {
 const compile = () => {
   const src = editor.getValue();
   localStorage.setItem('state', src);
+  console.clear();
   transform(src, {
     babelrc: false,
-    plugins: [babelPluginJSx],
+    plugins: [[babelPluginJSx, { transformOn: true }]],
+    ast: true,
   }, (err, result) => {
     if (!err) {
+      console.log('AST', result.ast);
       output.setValue(result.code);
     } else {
       output.setValue(err.message);
