a92ff91c160667423d6cadced28978917405b641
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2023-06-22 19:18:56 +0800

    feat(jsx-explorer): support dev debugging

--- packages/jsx-explorer/vite.config.ts
+++ packages/jsx-explorer/vite.config.ts
@@ -3,6 +3,11 @@ import MonacoEditorPlugin from 'vite-plugin-monaco-editor';
 import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@vue/babel-plugin-jsx': '@vue/babel-plugin-jsx/src/index.ts',
+    },
+  },
   plugins: [
     MonacoEditorPlugin({}),
     nodePolyfills({
