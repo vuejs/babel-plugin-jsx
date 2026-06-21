b03d012c2cfc2bae7bddc3ce3a46b9ef7828a45d
Kevin Deng <sxzz@sxzz.moe>
2026-06-21 23:51:04 +0800

    chore(jsx-explorer): fix polyfill

--- packages/jsx-explorer/vite.config.js
+++
@@ -1,8 +0,0 @@
-import VueJSX from '@vitejs/plugin-vue-jsx'
-import { defaultClientConditions, defineConfig } from 'vite'
-export default defineConfig({
-  resolve: {
-    conditions: ['vue-jsx-source', ...defaultClientConditions],
-  },
-  plugins: [VueJSX()],
-})
--- packages/jsx-explorer/vite.config.ts
+++ packages/jsx-explorer/vite.config.ts
@@ -3,7 +3,13 @@ import { defaultClientConditions, defineConfig } from 'vite'
 
 export default defineConfig({
   resolve: {
+    alias: {
+      'node:assert': 'assert',
+    },
     conditions: ['vue-jsx-source', ...defaultClientConditions],
+  },
+  define: {
+    'process.env.NODE_DEBUG': 'false',
   },
   plugins: [VueJSX()],
 })
