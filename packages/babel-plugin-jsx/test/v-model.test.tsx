ababa5c84bb9818fa3b3f459c182bd29d06a7c1e
Kevin Deng <sxzz@sxzz.moe>
2026-06-21 23:54:11 +0800

    chore: ignore lint

--- packages/babel-plugin-jsx/test/v-model.test.tsx
+++ packages/babel-plugin-jsx/test/v-model.test.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable unicorn/better-dom-traversing */
 import { mount, shallowMount } from '@vue/test-utils'
 import { expect, test } from 'vitest'
 import { defineComponent, type VNode } from 'vue'
