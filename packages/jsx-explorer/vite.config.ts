cbf34626edca754a162aa23d608beb3c72936f22
Kevin Deng <sxzz@sxzz.moe>
2026-06-21 23:18:47 +0800

    chore: fix source condition

--- packages/jsx-explorer/vite.config.ts
+++ packages/jsx-explorer/vite.config.ts
@@ -3,10 +3,7 @@ import { defaultClientConditions, defineConfig } from 'vite'
 
 export default defineConfig({
   resolve: {
-    conditions: ['dev', ...defaultClientConditions],
-  },
-  define: {
-    'process.env.BABEL_TYPES_8_BREAKING': 'false',
+    conditions: ['vue-jsx-source', ...defaultClientConditions],
   },
   plugins: [VueJSX()],
 })
